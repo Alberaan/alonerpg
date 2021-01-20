fbe641eeea2525b8e35b17613c1b636ea8787bc8
Alberaan <alberaan@gmail.com>
2021-01-20 22:11:32 +0100

    Integrated tables and first backend functionalities. You can use the "command" url parameter to execute the tool

--- alonerpg/views.py
+++ alonerpg/views.py
@@ -1,11 +1,28 @@
-
+from .tablesreader import *
 from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
 
     template_name = "home.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['salutation'] = "Hello world"
-        return context
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        #context['parametros'] = request.GET["ls"]
+        command = request.GET["command"]
+        tables = get_tables()
+        result = dynamic_call(tables, command)
+        context["message"] = result.message
+        context["data"] = get_text(result.data)
+
+        return self.render_to_response(context)
+
+def get_text(data):
+    text = []
+    if isinstance(data, str):
+        text.append(data)
+    if isinstance(data, list):
+        for table in data:
+            if isinstance(table, Table):
+                text.append(str(table.index) + ": (" + table.system + ") " + table.filename)
+    return text
+    
